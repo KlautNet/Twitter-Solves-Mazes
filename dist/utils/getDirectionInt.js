8ed9d4a3d94462be60903999385265d7a361912a
KlautNet <63518974+KlautNet@users.noreply.github.com>
2022-12-16 00:14:28 +0100

    Delete dist directory

--- dist/utils/getDirectionInt.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=getDirectionInt.js.map
--- dist/utils/sleep.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-function sleep(delay) {
-    return new Promise(resolve => setTimeout(resolve, delay));
-}
-exports.sleep = sleep;
-//# sourceMappingURL=sleep.js.map
